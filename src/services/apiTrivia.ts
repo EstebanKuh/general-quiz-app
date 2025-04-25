18bc6604d650ed8be296ce40095a83e4bec5d93a
Ekuh <esteban.kuh@gmail.com>
2025-04-24 21:58:30 -0600

    New Features
    Add get Categories
    Add new attributes to get questions

--- src/services/apiTrivia.ts
+++ src/services/apiTrivia.ts
@@ -1,7 +1,18 @@
-import { Question } from "../types";
+import { Question, APICategoryResponse } from "../types";
 
-export const fetchQuestions = async (amount = 10): Promise<Question[]> => {
-  const res = await fetch(`https://opentdb.com/api.php?amount=${amount}&type=multiple`);
+export const fetchQuestions = async (
+  amount = 10,
+  category = '9',
+  difficult = 'easy',
+  type = 'multiple'
+): Promise<Question[]> => {
+  const res = await fetch(`https://opentdb.com/api.php?amount=${amount}&type=${type}&category=${category}&difficulty=${difficult}`);
   const data = await res.json();
   return data.results;
+};
+
+export const fetchCategories = async(): Promise<APICategoryResponse> => {
+  const res = await fetch(`https://opentdb.com/api_category.php`);
+  const data = await res.json();
+  return data;
 };
